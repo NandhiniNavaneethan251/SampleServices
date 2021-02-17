98df22f58a6c9b80550f9402a3e1a1b1f0d09c99
NandhiniNavaneethan251 <69723797+NandhiniNavaneethan251@users.noreply.github.com>
2021-02-17 12:27:40 +0530

    basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import students from './_files/students.json';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SampleServices';
+  title = 'json-file-read-angular';
+  public studentList:{id:number,name:string, Age:number}[] = students;
 }
